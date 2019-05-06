63501509a5c44b3b78f8627d4fa141d068f17288
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-05-05 20:27:15 -0700

    Update to twitter and add create-react-app

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const Twitter = require('twitter');
+const key = require('./config/twitter');
 const app = express();
+const client = new Twitter({
+  consumer_key: key.CONSUMER_KEY,
+  consumer_secret: key.CONSUMER_SECRET,
+  access_token_key: key.ACCESS_TOKEN,
+  access_token_secret: key.TOKEN_SECRET
+});
+
 
 app.get('/', (req, res) => {
-  fetch('https://api.twitter.com/1.1/trends/place.json?id=23424977')
-    .then((response) => response.json())
-    .then((data) => res.json(data));
+  client.get('trends/place', { id: 23424977 }, (err, data, response) => {
+    res.json(data);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
