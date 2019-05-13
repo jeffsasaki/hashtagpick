a3dffd955a7aae31026446023211b9a76a8383aa
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-05-13 13:32:28 -0700

    Fix path variable

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Twitter = require('twitter');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
