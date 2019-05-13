ad1ea17a8ee72ad8879713b95f816500d060bf06
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-05-13 00:26:12 -0700

    Restore environment var PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Twitter = require('twitter');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
