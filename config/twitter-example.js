9d6b54718ae7defc2b9f111871384456a68bc7a3
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-05-12 23:29:34 -0700

    Update gitignore for twitter keys

--- config/twitter-example.js
+++
@@ -1,7 +0,0 @@
-// Add your Twitter API keys here and rename the file twitter.js
-module.exports = {
-  CONSUMER_KEY: 'YourConsumerKey',
-  CONSUMER_SECRET: 'YourConsumerSecret',
-  ACCESS_TOKEN: 'YourAccessToken',
-  TOKEN_SECRET: 'YourTokenSecret'
-};
---
+++ config/twitter.js
@@ -0,0 +1,6 @@
+module.exports = {
+  CONSUMER_KEY: process.env.CONSUMER_KEY,
+  CONSUMER_SECRET: process.env.CONSUMER_SECRET,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  TOKEN_SECRET: process.env.TOKEN_SECRET
+};
