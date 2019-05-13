e33b091a36e4e79de60d6ae7b78ca1e779583b25
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-05-12 23:35:55 -0700

    Temporary hardcoding port 5000 for server

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.get('/trending', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
