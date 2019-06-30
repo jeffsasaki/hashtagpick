d9cd2d4a8cd83e1eef2f14b9ba3bc2e1b47c1f2a
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-06-29 20:47:28 -0700

    target shorter hashtags

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Twitter = require('twitter');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;z
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -19,7 +19,7 @@ app.get('/api/trending', (req, res) => {
     let tagset = new Set();
     let i = 0;
     while (tagset.size < 30 && i < 50) {
-      if (output[i]['name'].replace(/\W/g, '').length <= 20) {
+      if (output[i]['name'].replace(/\W/g, '').length <= 16) {
         tagset.add('#' + output[i]['name'].replace(/\W/g, ''));
       }
       i++;
