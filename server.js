e424af4bc8f374da5c26ac3b92fbd159e148f842
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2019-06-29 20:49:09 -0700

    syntax fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Twitter = require('twitter');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;z
+const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
