5d487f4170936f2a1b11554477a4705a20fdbe58
Jeffrey Sasaki <jeffreysasaki@Jeffreys-MacBook-Pro.local>
2020-10-10 21:31:11 -0700

    Add Google trend api

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const Twitter = require('twitter');
+const googleTrends = require('google-trends-api');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -13,35 +16,59 @@ const client = new Twitter({
   access_token_secret: process.env.TOKEN_SECRET
 });
 
-app.get('/api/trending', (req, res) => {
-  client.get('trends/place', { id: 23424977 }, (err, data, response) => {
-    const trends = data[0]['trends'];
-    const output = [];
-    const tagset = new Set();
-    let i = 0;
+const getGoogleTrends = () => {
+  return googleTrends
+    .dailyTrends({ geo: 'US' })
+    .then(data => {
+      const output = [];
+      const parsedData = JSON.parse(data).default.trendingSearchesDays[0].trendingSearches;
+      parsedData.forEach(val => output.push(val.title.query));
+      return output;
+    });
+};
 
-    for(let j = 0; j < trends.length; j++) {
-      output.push(trends[j]['name']);
-    }
-    output.sort((a, b) => a.length - b.length);
+const getTwitterTrends = () => {
+  return client.get('trends/place', { id: 23424977 })
+    .then(data => {
+      const trends = data[0]['trends'];
+      const output = [];
+      for(let j = 0; j < trends.length; j++) {
+        output.push(trends[j]['name']);
+      }
+      return output;
+    });
+};
 
-    while (tagset.size < 30 && i < output.length) {
-      if (typeof output[i] === 'undefined') {
+app.get('/api/trending', (req, res) => { 
+  const promises = [
+    getGoogleTrends(),
+    getTwitterTrends()
+  ];
+  Promise.all(promises)
+    .then(data => {
+      const output = data[0].concat(data[1]);
+      const tagset = new Set();
+      let i = 0;
+
+      output.sort((a, b) => a.length - b.length);
+      while (tagset.size < 30 && i < output.length) {
+        if (typeof output[i] === 'undefined') {
+          i++;
+          continue;
+        }
+        const hashtag = output[i].replace(/\W/g, '');
+  
+        if (hashtag.length > 0 && hashtag[0] !== '_') {
+          tagset.add(`#${hashtag}`);
+          process.stdout.write(`✅ ${output[i]}\n`);
+        } else {
+          process.stdout.write(`❌ ${output[i]}\n`);
+        }
         i++;
-        continue;
       }
-      const hashtag = output[i].replace(/\W/g, '');
-
-      if (hashtag.length > 0 && hashtag[0] !== '_') {
-        tagset.add(`#${hashtag}`);
-        process.stdout.write(`✅ ${output[i]}\n`);
-      } else {
-        process.stdout.write(`❌ ${output[i]}\n`);
-      }
-      i++;
-    }
-    res.json(Array.from(tagset));
-  });
+      res.json(Array.from(tagset));
+    })
+    .catch(err => console.error(err));
 });
 
 if(process.env.NODE_ENV === 'production') {
